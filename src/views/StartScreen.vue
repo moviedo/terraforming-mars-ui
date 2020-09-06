d314324bc8f75c6db9e0da1f3d0c90ab5fcc8a4f
Mauro Oviedo <mauro.oviedo@protonmail.com>
2020-09-06 15:11:10 -0600

    Initial.

---
+++ src/views/StartScreen.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="start-screen">
+    <div v-i18n class="start-screen-links">
+      <div class="start-screen-header start-screen-link--title">
+        <div class="start-screen-title-top">TERRAFORMING</div>
+        <div class="start-screen-title-bottom">MARS</div>
+      </div>
+
+      <a class="start-screen-link start-screen-link--new-game" href="/new-game" v-i18n>New game</a>
+      <a class="start-screen-link start-screen-link--solo" href="/solo" v-i18n>Solo challenge</a>
+      <a class="start-screen-link start-screen-link--cards-list" href="https://ssimeonoff.github.io/cards-list" v-i18n>Cards list</a>
+      <a class="start-screen-link start-screen-link--board-game" href="https://boardgamegeek.com/boardgame/167791/terraforming-mars" v-i18n>Board game</a>
+      <a class="start-screen-link start-screen-link--about" href="https://github.com/bafolts/terraforming-mars" v-i18n>About us</a>
+      <a class="start-screen-link start-screen-link--changelog" href="https://github.com/bafolts/terraforming-mars/wiki/Changelog" v-i18n>Whats new?</a>
+      <a class="start-screen-link start-screen-link--chat" href="https://discord.gg/fWXE53K" target="_blank" v-i18n>Join us on Discord</a>
+
+      <div class="start-screen-header start-screen-link--languages">
+        <LanguageSwitcher/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+// import { LanguageSwitcher } from "./LanguageSwitcher";
+
+export default Vue.extend({
+  name: 'StartScreen',
+  components: {
+
+  },
+});
+</script>
+
+<style>
+.topmost-start-screen {
+  margin: 0px;
+  padding-bottom: 40px;
+  background: url(../assets/stars.jpg);
+  min-height: 100%;
+}
+</style>
+
+<style lang="less" scoped>
+.start-screen {
+  width: 400px;
+  margin: 0 auto;
+}
+
+.start-screen-game-title {
+  color: #daab63;
+  padding: 40px 0 10px 0;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.start-screen-lang-switch {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.start-screen-links {
+  margin: 0 auto;
+  width: 370px;
+  padding-top: 50px;
+}
+
+.start-screen-link {
+  text-decoration: none;
+  cursor: pointer;
+  box-shadow: inset 0 0 0px 1px #333;
+  height: 90px;
+  line-height: 130px;
+  border-radius: 10px;
+  color: #eee;
+  text-shadow: 0 0 2px black, 0 2px 1px black;
+  padding-left: 17px;
+  width: 369px;
+  height: 90px;
+  display: block;
+  margin-bottom: 5px;
+  font-size: 26px;
+  text-transform: uppercase;
+  font-weight: bold;
+  letter-spacing: 1px;
+
+  &:hover, &:visited, &:active, &:focus {
+    color: #eee;
+    text-decoration: none;
+  }
+
+  &:hover {
+    filter: brightness(120%);
+  }
+}
+
+.start-screen-header {
+  box-shadow: inset 0 0 20px 20px black;
+  font-family: Prototype;
+  line-height: 90px;
+  font-size: 16px;
+  width: 369px;
+  height: 90px;
+  line-height: 30px;
+  color: #daab63;
+  display: block;
+  margin-bottom: 5px;
+  margin-left: 5px;
+  text-shadow: 0 0 2px black, 0 2px 2px black;
+  letter-spacing: 1px
+}
+
+.start-screen-title-top {
+  transform: scaleX(2);
+  margin-left: 162px;
+  padding-top: 15px;
+}
+
+.start-screen-title-bottom {
+  transform: scaleX(2);
+  font-size: 30px;
+  margin-left: 190px;
+}
+
+.start-screen-link--title {
+  background-image: url(../assets/planet1.png);
+}
+
+.start-screen-link--new-game {
+  background-image: url(../assets/planet2.png);
+}
+
+.start-screen-link--solo {
+  background-image: url(../assets/planet3.png);
+}
+
+.start-screen-link--cards-list {
+  background-image: url(../assets/planet4.png);
+}
+
+.start-screen-link--board-game {
+  background-image: url(../assets/planet5.png);
+}
+
+.start-screen-link--about {
+  background-image: url(../assets/planet6.png);
+}
+
+.start-screen-link--changelog {
+  background-image: url(../assets/planet7.png);
+}
+
+.start-screen-link--chat {
+  background-image: url(../assets/planet8.png);
+}
+
+.start-screen-link--languages {
+  background-image: url(../assets/planet9.png);
+  padding-top: 30px;
+}
+</style>
